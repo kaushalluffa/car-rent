61658b3c703bf5d664612d8b171cb4d7fe16e590
kaushalluffa <webdevelopment852@gmail.com>
2023-12-19 14:35:34 -0500

    changes for the project and the custom theme colors added

--- README.md
+++ README.md
@@ -1,70 +1,12 @@
-## vite-mui-ts boilerplate
 
-TypeScript + React + Redux + MUI + RRD + ESLint + Prettier
+# Morent - Car Renting
 
-![vite-mui-ts](https://i.ibb.co/j8JSrhV/Screenshot-2023-07-06-121352.png)
-
-### Getting Started
-
-#### Clone the repo
-
-```
-npx degit emre-cil/vite-mui-ts my-app
-```
-
-```
-cd my-app
-```
-
-#### Install Dependencies
-
-```
-pnpm install
-```
-
-#### Run
-
-```
-pnpm dev
-```
-
-#### Paths
-
-Application using absolute paths
-Example: '@/components/Counter/Counter';
-
-if you don't want to use you can remove these lines from
-
-> vite.config.ts
-
-```
- resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-```
-
-> tsconfig.json
-
-```
-"paths": {
-      "@/*": ["./*"]
-    }
-```
+The application is an MVP of a car renting webiste
 
 
-### Scripts
 
-| Script        | Description                        |
-| ------------- | ---------------------------------- |
-| pnpm dev      | Runs the application.              |
-| pnpm build    | Create builds for the application. |
-| pnpm preview  | Runs the Vite preview              |
-| pnpm lint     | Display eslint errors              |
-| pnpm lint:fix | Fix the eslint errors              |
-| pnpm format   | Runs prettier for all files        |
-| pnpm test     | Run tests                          |
+## Acknowledgements
 
-### Check List
-````
+ - [Figma Template Link](https://www.figma.com/file/uVf1iwNrw23P0FbEMTyffQ/Car-Rent-Mobile-App-Design---UI-KIT-(Community))
+ - [Pickolab Studio - Figma designer](https://www.figma.com/@pickolabstudio)
+
--- src/themes/AppThemeProvider.tsx
+++ src/themes/AppThemeProvider.tsx
@@ -8,271 +8,115 @@ type Props = {
 };
 
 declare module '@mui/material/styles' {
-  // index signature typegradients
-
-  interface TypeGradient {
-    [key: string]: string;
+  interface ColorOptions {
+    900: string;
+    800: string;
+    700: string;
+    600: string;
+    500: string;
+    400: string;
+    300: string;
+    200: string;
+    100: string;
   }
-
-  interface Palette {
-    gradient: TypeGradient;
-    text: TypeText2;
+  interface Theme {
+    cr: {
+      primary: ColorOptions;
+      info: ColorOptions;
+      success: ColorOptions;
+      warning: ColorOptions;
+      error: ColorOptions;
+      secondary: ColorOptions;
+    };
   }
-  interface TypeColor {
-    Darkest?: string;
-    Darker?: string;
-    Dark?: string;
-    Base?: string;
-    Light?: string;
-    Lighter?: string;
-    Lightest?: string;
-    White?: string;
-  }
-  interface TypeText2 {
-    primary: string;
-    secondary: string;
-    disabled: string;
-  }
-  interface PaletteOptions {
-    gradient: TypeGradient;
-    Ink: TypeColor;
-    Sky: TypeColor;
-    Red: TypeColor;
-    Green: TypeColor;
-  }
-
-  interface TypeBackground {
-    opposite: string;
-  }
-  interface TypographyVariants {
-    CTA1: React.CSSProperties;
-    CTA2: React.CSSProperties;
-    CTA3: React.CSSProperties;
-    body3: React.CSSProperties;
-    body4: React.CSSProperties;
-    Body1: React.CSSProperties;
-    Body2: React.CSSProperties;
-    Body3: React.CSSProperties;
-    Body1Medium: React.CSSProperties;
-    Body1SemiBold: React.CSSProperties;
-  }
-
-  // allow configuration using `createTheme`
-  interface TypographyVariantsOptions {
-    CTA1?: React.CSSProperties;
-    CTA2?: React.CSSProperties;
-    CTA3?: React.CSSProperties;
-    body3?: React.CSSProperties;
-    body4?: React.CSSProperties;
-    Body1?: React.CSSProperties;
-    Body2?: React.CSSProperties;
-    Body3?: React.CSSProperties;
-    Body1Medium?: React.CSSProperties;
-    Body1SemiBold?: React.CSSProperties;
+  interface ThemeOptions {
+    cr?: {
+      primary?: ColorOptions;
+      info?: ColorOptions;
+      success?: ColorOptions;
+      warning?: ColorOptions;
+      error?: ColorOptions;
+      secondary?: ColorOptions;
+    };
   }
 }
-declare module '@mui/material/Typography' {
-  interface TypographyPropsVariantOverrides {
-    CTA1: true;
-    CTA2: true;
-    CTA3: true;
-    body3: true;
-    body4: true;
-    Body1: true;
-    Body2: true;
-    Body3: true;
-    Body1Medium: true;
-    Body1SemiBold: true;
-  }
-}
+
 export const AppThemeProvider: React.FC<Props> = ({ children }) => {
   const mode = useAppSelector((state) => state.user.mode);
   const theme = responsiveFontSizes(
     createTheme({
       palette: {
         mode: mode as PaletteMode,
+      },
+      cr: {
         primary: {
-          main: '#1c9c7c',
+          900: '#0A196F',
+          800: '#102587',
+          700: '#1a37a7',
+          600: '#264bc8',
+          500: '#3563e9',
+          400: '#658df1',
+          300: '#85a8f8',
+          200: '#aec8fc',
+          100: '#d6e4fd',
         },
         secondary: {
-          main: '#9DF3C4',
+          900: '#040815',
+          800: '#080c19',
+          700: '#0d121f',
+          600: '#131825',
+          500: '#1a202c',
+          400: '#596780',
+          300: '#90a3bf',
+          200: '#c3d4e9',
+          100: '#e0e9f4',
         },
-        Ink: {
-          Darkest: '#000000',
-          Darker: '#222222',
-          Dark: '#303437',
-          Base: '#404446',
-          Light: '#6C7072',
-          Lighter: '#72777A',
+        success: {
+          900: '#3b6506',
+          800: '#4c7a0b',
+          700: '#659711',
+          600: '#7fb519',
+          500: '#9cd323',
+          400: '#bce455',
+          300: '#d3f178',
+          200: '#e8faa6',
+          100: '#f5fcd2',
         },
-        Sky: {
-          Dark: '#979C9E',
-          Base: '#CDCFD0',
-          Light: '#E3E5E5',
-          Lighter: '#F2F4F5',
-          Lightest: '#F7F9FA',
-          White: '#FFFFFF',
+        warning: {
+          900: '#7a4d0b',
+          800: '#936312',
+          700: '#b7821d',
+          600: '#dba32a',
+          500: '#ffc73a',
+          400: '#ffd96b',
+          300: '#ffe488',
+          200: '#ffefb0',
+          100: '#fff8d7',
         },
-
-        Red: {
-          Darkest: '#6B0206',
-          Base: '#E8282B',
-          Light: '#F94739',
-          Lighter: '#FF9898',
-          Lightest: '#FFE5E5',
+        error: {
+          900: '#7a0619',
+          800: '#930b16',
+          700: '#b71112',
+          600: '#db2719',
+          500: '#ff4423',
+          400: '#ff7f59',
+          300: '#ffa37a',
+          200: '#ffc8a6',
+          100: '#ffe7d3',
         },
-
-        Green: {
-          Darkest: '#0A4C0A',
-          Base: '#0F8B0F',
-          Light: '#1EB01E',
-          Lighter: '#7FF77F',
-          Lightest: '#E5FFE5',
-        },
-        background: {
-          default: mode === 'dark' ? '#000000' : '#FCFBFA',
-          opposite: mode === 'dark' ? '#FCFBFA' : '#000000',
-          paper: mode === 'dark' ? '#131313' : '#FCFCFC',
-        },
-        text: {
-          primary: mode === 'dark' ? '#FFFFFF' : '#000000',
-          secondary: '#999999',
-          disabled: '#C3C1BD',
-        },
-
-        grey: {
-          50: mode === 'dark' ? 'hsl(0, 0%, 10%)' : 'hsl(0, 5%, 95%)',
-          100: mode === 'dark' ? 'hsl(0, 0%, 20%)' : 'hsl(0, 0%, 90%)',
-          200: mode === 'dark' ? 'hsl(0, 0%, 30%)' : 'hsl(0, 0%, 80%)',
-          300: mode === 'dark' ? 'hsl(0, 0%, 40%)' : 'hsl(0, 0%, 70%)',
-          400: mode === 'dark' ? 'hsl(0, 0%, 50%)' : 'hsl(0, 0%, 60%)',
-          500: mode === 'dark' ? 'hsl(0, 0%, 60%)' : 'hsl(0, 0%, 50%)',
-          600: mode === 'dark' ? 'hsl(0, 0%, 70%)' : 'hsl(0, 0%, 40%)',
-          700: mode === 'dark' ? 'hsl(0, 0%, 80%)' : 'hsl(0, 0%, 30%)',
-          800: mode === 'dark' ? 'hsl(0, 0%, 90%)' : 'hsl(0, 0%, 20%)',
-          900: mode === 'dark' ? 'hsl(0, 5%, 95%)' : 'hsl(0, 0%, 10%)',
-        },
-        gradient: {
-          bronze: 'linear-gradient(180deg, #9C6D3E 0%, #E8C8A9 100%)',
-          silver: 'linear-gradient(180deg, #808080 0%, #DFDFDF 100%)',
-          gold: 'linear-gradient(180deg, #A3873C 0%, #E3D294 100%)',
+        info: {
+          900: '#102e7a',
+          800: '#1a4393',
+          700: '#2a60b7',
+          600: '#3d81db',
+          500: '#54a6ff',
+          400: '#7ec2ff',
+          300: '#98d3ff',
+          200: '#bae5ff',
+          100: '#dcf3ff',
         },
       },
-
-      typography: {
-        fontFamily: 'Dosis, sans-serif',
-
-        h1: {
-          fontSize: '26px',
-          fontWeight: '600',
-          // lineHeight: '33px',
-        },
-        h2: {
-          fontSize: '22px',
-          fontWeight: '600',
-          // lineHeight: '28px',
-        },
-        h3: {
-          fontSize: '20px',
-          fontWeight: '600',
-          // lineHeight: '25px',
-        },
-        h4: {
-          fontSize: '18px',
-          fontWeight: '600',
-          // lineHeight: '23px',
-        },
-        h5: {
-          fontSize: '16px',
-          fontWeight: '500',
-          // lineHeight: '20px',
-        },
-
-        CTA1: {
-          fontSize: '28px',
-          fontWeight: '500',
-          // lineHeight: '35px',
-        },
-        CTA2: {
-          fontSize: '18px',
-          fontWeight: '500',
-          // lineHeight: '23px',
-        },
-        CTA3: {
-          fontSize: '16px',
-          fontWeight: '400',
-          // lineHeight: '20px',
-        },
-        Body1: {
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '14px',
-          fontWeight: '400',
-          // lineHeight: '18px',
-        },
-        Body2: {
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '13px',
-          fontWeight: '400',
-          // lineHeight: '16px',
-        },
-        Body3: {
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '12px',
-          fontWeight: '400',
-          // lineHeight: '14px',
-        },
-        Body1Medium: {
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '14px',
-          fontWeight: '500',
-          // lineHeight: '17px',
-        },
-        Body1SemiBold: {
-          fontFamily: 'Lato, sans-serif',
-          fontSize: '14px',
-          fontWeight: '600',
-          // lineHeight: '17px',
-        },
-        body3: {
-          fontSize: '12px',
-          // lineHeight: '16px',
-          display: 'block',
-        },
-        body4: {
-          fontSize: '10px',
-          // lineHeight: '14px',
-          display: 'block',
-        },
-      },
-      components: {
-        MuiCssBaseline: {
-          styleOverrides: {
-            body: {
-              // ---CSS BODY--- \\
-            },
-          },
-        },
-        MuiLink: {
-          styleOverrides: {
-            root: {
-              cursor: 'pointer',
-              textDecoration: 'none',
-              lineHeight: '16px',
-              transition: 'all 0.1s ease-in-out',
-              '&:hover': {
-                opacity: 0.8,
-              },
-            },
-          },
-        },
-        MuiIconButton: {
-          styleOverrides: {
-            root: {
-              aspectRatio: '1/1',
-            },
-          },
-        },
-      },
+    
     }),
   );
 
